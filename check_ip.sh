433a62efc105cea1fae02c09e8f239efbc2a4dbf
RANRAN-PC\Jingxiang <1585615244@qq.com>
2020-07-29 16:05:50 +0800

    写了一个检测ip的脚本

---
+++ check_ip.sh
@@ -0,0 +1,11 @@
+#老板交给我的任务，写一个检测ip是在线的脚本，干
+for i in {150..220}
+do
+        ping -c 4 -i 0.2 -W 1 192.168.1.$i &> /dev/null
+        if [ $? -eq 0 ];then
+                echo "192.168.1.$i is up!"
+        else
+                echo "192.168.1.$i is down!"
+        fi
+done
+
